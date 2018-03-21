7b8c7c794c5ae0d0d3304db0e166638a3f35c9ea
David Morrill <judimorrill@judis-mbp.lan>
2018-03-20 23:42:53 -0700

    changes

---
+++ src/components/footer/footer.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+    <header className="footer">
+        <p>Clicky Game by David</p>
+    </header>
+);
+
+export default Footer;
